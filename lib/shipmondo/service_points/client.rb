7399e3395b8b963298995ad5084a855569dc80ff
Mads Lundholm <mads@madslundholm.dk>
2022-08-03 13:53:57 +0200

    Add support for quantity in the pickup points endpoint

--- lib/shipmondo/service_points/client.rb
+++ lib/shipmondo/service_points/client.rb
@@ -9,8 +9,8 @@ module Shipmondo
         @http = http
       end
 
-      def pickup_points(carrier_code:, address: nil, city: nil, zipcode:, country_code:)
-        http.get('pickup_points', carrier_code:, address:, city:, zipcode:, country_code:).body.map do |json|
+      def pickup_points(carrier_code:, address: nil, city: nil, zipcode:, country_code:, quantity: nil)
+        http.get('pickup_points', carrier_code:, address:, city:, zipcode:, country_code:, quantity:).body.map do |json|
           ServicePoint.new(json)
         end
       end
--- spec/shipmondo/service_points/client_spec.rb
+++ spec/shipmondo/service_points/client_spec.rb
@@ -9,10 +9,11 @@ describe Shipmondo::ServicePoints::Client do
     let(:city) { 'Aarhus C' }
     let(:zipcode) { '8000' }
     let(:country_code) { 'DK' }
+    let(:quantity) { 5 }
 
     before do
       stub_request(:get, 'https://app.shipmondo.com/api/public/v3/pickup_points')
-        .with(query: { carrier_code:, address:, city:, zipcode:, country_code: })
+        .with(query: { carrier_code:, address:, city:, zipcode:, country_code:, quantity: })
         .to_return(headers: { content_type: 'application/json' },
                    body: JSON.generate([
                      { id: '95558', name: 'Påskeløkkens Købmand', address: 'Paaskeløkkevej 11', zip_code: '5000', city: 'Odense C', country: 'DK' }
@@ -20,7 +21,7 @@ describe Shipmondo::ServicePoints::Client do
     end
 
     it 'can list pickup points neares the given address' do
-      pickup_points = subject.pickup_points(carrier_code:, address:, city:, zipcode:, country_code:)
+      pickup_points = subject.pickup_points(carrier_code:, address:, city:, zipcode:, country_code:, quantity:)
 
       expect(pickup_points).to all(be_instance_of(Shipmondo::ServicePoints::ServicePoint))
     end
